397d0d6f346c6b53fd51a65d2cb2cf54700d6248
Nataliia <nata.buchna@gmail.com>
2021-08-05 19:29:57 +0300

    toggle method

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 let isShort = false;
+
 class Timer {
     constructor({}) {
-        document.getElementById('clock').addEventListener("click", function () {
-            isShort = !isShort;
-        });
+        this.toggle();
     };
 
     render(){
@@ -14,18 +13,24 @@ class Timer {
         if (minutes < 10) minutes = '0' + minutes;
         let seconds = fullDate.getSeconds();
         if (seconds < 10) seconds = '0' + seconds;
-        if (isShort) {
+        if(isShort){
             document.getElementById('clock').innerHTML = hours + ':' + minutes;
         } else {
             document.getElementById('clock').innerHTML = hours + ':' + minutes + ':' + seconds;
         }
+    };
 
+    toggle(){
+        document.getElementById('clock').addEventListener("click", function () {
+            isShort = !isShort;
+        });
     };
 
     start() {
         this.render();
         setInterval(() => this.render(), 1000);
-    }
+    };
 }
+
 let timer = new Timer({timer: 'hours : minutes : seconds'});
 timer.start();
